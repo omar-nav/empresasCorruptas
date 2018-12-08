eafdd978fa13c77a97555319f2186fab46a5d082
Omar Cesar <omar-nav@users.noreply.github.com>
2018-12-08 13:10:11 -0600

    TODO agregar valores de totales y ratio

--- map.js
+++ map.js
@@ -12,7 +12,7 @@ const numberWithCommas = (from) => {
     return from.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
-function choroplethize(d) {
+function choroplethizeTotal(d) {
     return d > 162 ? '#990000' :
         d > 80 ? '#d7301f' :
             d > 54 ? '#ef6548' :
@@ -21,27 +21,47 @@ function choroplethize(d) {
                      d > 5 ? '#fdd49e' :
                         '#fef0d9';
 }
-function styleSecuestros(feature) {
+
+function choroplethizeRatio(d) {
+    return d > .8 ? '#990000' :
+    d > .7 ? '#d7301f' :
+        d > .5 ? '#ef6548' :
+            d > .3 ? '#fc8d59' :
+             d > .2 ? '#fdbb84' :
+                 d > .1 ? '#fdd49e' :
+                    '#fef0d9';
+}
+}
+function styleEmpresasTotal(feature) {
     return {
         weight: .75,
         opacity: 0.5,
         color: 'grey',
         dashArray: '0',
         fillOpacity: 0.9,
-        fillColor: choroplethize(feature.properties._Total)
+        fillColor: choroplethizeTotal(feature.properties._Total)
     }
 }
 
-//
-
-// function label(feature, layer){
-//     if(feature.properties.refactor_4 != null){
-//         layer.bindTooltip(feature.properties.refactor_4 + ' per 100,000',{direction: 'center'},{permanent: true},{opacity: .1});
-//         layer.bindPopup('Country:   ' + feature.properties.SOVEREIGNT + '<br>Total Homicides:   '+ feature.properties.refactor_3 + '<br>Homicide Rate:   '+ feature.properties.refactor_4 + ' per 100,000')
-//     }
-// }
-var Secuestros2015Layer = L.geoJSON([secuestros2015], {
-    style: styleSecuestros,
+function styleEmpresasRatio(feature) {
+    return {
+        weight: .75,
+        opacity: 0.5,
+        color: 'grey',
+        dashArray: '0',
+        fillOpacity: 0.9,
+        fillColor: choroplethizeRatio(feature.properties.Ratio)
+    }
+}
+var EmpresasTotalesLayer = L.geoJSON([empresas], {
+    style: styleEmpresasTotal,
+     onEachFeature: geojsonPopup,
+    pointToLayer: function (feature, latlng){
+        return L.marker(latlng);
+    }
+});
+var EmpresasRatioLayer = L.geoJSON([empresas], {
+    style: styleEmpresasRatio,
      onEachFeature: geojsonPopup,
     pointToLayer: function (feature, latlng){
         return L.marker(latlng);
@@ -50,23 +70,25 @@ var Secuestros2015Layer = L.geoJSON([secuestros2015], {
 
 function geojsonPopup(feature, layer){
     if(feature.properties.NOMGEO != null){
-        layer.bindPopup('Estado:   ' + feature.properties.NOMGEO + '<br>Secuestros en 2015:   '+ feature.properties._Total);
+        layer.bindPopup('Estado:   ' + feature.properties.NOMGEO + '<br>Total de empresas:   '+ feature.properties._Total + '<br>Ratio de corrupción:   '+ feature.properties.Ratio);
     }
 }
 
-Secuestros2015Layer.addTo(mymap);
+EmpresasTotalesLayer.addTo(mymap);
 var featureLayers = {
-    "2015 Secuestros": Secuestros2015Layer
+    "Total de Empresas": EmpresasTotalesLayer,
+    "Ratio de Corrupción": EmpresasRatioLayer
 };
 var geojson = L.control.layers(featureLayers).addTo(mymap);
 
 // LEGEND STARTS HERE
-var Secuestros2015Legend = L.control({ position: 'bottomright' });
+var EmpresasTotalesLegend = L.control({ position: 'bottomright' });
+var EmpresasRatioLegend = L.control({ position: 'bottomright' }); 
 
-Secuestros2015Legend.onAdd = function (mymap) {
+EmpresasTotalesLegend.onAdd = function (mymap) {
     var div = L.DomUtil.create('div', 'info legend'),
     grades = [0, 5, 11, 24, 80],
-    labels = ['Suma de Secuestros en 2015 por estado'],
+    labels = ['Suma de Empresas con casos de corrupción'],
     fromLabel, from, toLabel, to;
     for (var i = 0; i < grades.length-1; i++) {
         from = grades[i];
@@ -74,27 +96,42 @@ Secuestros2015Legend.onAdd = function (mymap) {
         to = grades[i + 1];
         toLabel = numberWithCommas(grades[i + 1]);
         labels.push(
-            '<i style="background:' + choroplethize(from + 1) + '"></i> ' +
+            '<i style="background:' + choroplethizeTotal (from + 1) + '"></i> ' +
             fromLabel + (toLabel ? ' - ' + toLabel : ' - 162'));
-            // last value of 1 billion on line 76 not currently being used
-            // kept as placemarker 
     }
     div.innerHTML = labels.join('<br>');
     return div;
 };
-Secuestros2015Legend.addTo(mymap);
-let currentLegend = Secuestros2015Legend;
+EmpresasRatioLegend.onAdd = function (mymap) {
+    var div = L.DomUtil.create('div', 'info legend'),
+    grades = [.1,.2,.3,.4,.5,.7],
+    labels = ['Ratio de corrupción'],
+    fromLabel, from, toLabel, to;
+    for (var i = 0; i < grades.length-1; i++) {
+        from = grades[i];
+        fromLabel = numberWithCommas(grades[i]);
+        to = grades[i + 1];
+        toLabel = numberWithCommas(grades[i + 1]);
+        labels.push(
+            '<i style="background:' + choroplethizeRatio(from + 1) + '"></i> ' +
+            fromLabel + (toLabel ? ' - ' + toLabel : ' - .8'));
+    }
+    div.innerHTML = labels.join('<br>');
+    return div;
+};
+EmpresasTotalesLegend.addTo(mymap);
+let currentLegend = EmpresasTotalesLegend;
 
 // LEGEND Box
 mymap.on('baselayerchange', function (eventLayer) {
-    if (eventLayer.name === '2015 Secuestros') {
+    if (eventLayer.name === 'Total de Empresas') {
         mymap.removeControl(currentLegend);
-        currentLegend = Secuestros2015Legend;
-        Secuestros2015Legend.addTo(mymap);
+        currentLegend = EmpresasTotalesLegend;
+        EmpresasTotalesLegend.addTo(mymap);
     }
-    // else if (eventLayer.name === '2017 Corruption') {
-    //     mymap.removeControl(currentLegend);
-    //     currentLegend = Corruption2017Legend;
-    //     Corruption2017Legend.addTo(mymap);
-    // }
+    else if (eventLayer.name === 'Ratio de Corrupción') {
+        mymap.removeControl(currentLegend);
+        currentLegend = EmpresasRatioLegend;
+        EmpresasRatioLegend.addTo(mymap);
+    }
 });
