9da6c450d21b651d60c9dadce8e60dc64dd71a73
Omar Cesar <omar-nav@users.noreply.github.com>
2018-12-08 14:42:32 -0600

    TODO agregar valores de totales y ratio

--- multiLineChart.js
+++ multiLineChart.js
@@ -33,7 +33,7 @@ var svg = d3.select("#graph")
         .attr("class", "svg");
 
 // Import the CSV data
-d3.csv("Example4.csv", function(error, data) {
+d3.csv("multiLineChart.csv", function(error, data) {
   if (error) throw error;
   
    // Format the data
@@ -45,17 +45,24 @@ d3.csv("Example4.csv", function(error, data) {
   });
 
   var nest = d3.nest()
-	  .key(function(d){
-	    return d.Fruit;
-	  })
-	  .key(function(d){
-	  	return d.Year;
-	  })
+	    .key(function(d){
+	    	return d.Fruit;
+	    })
+		.rollup(function(leaves){
+            var max = d3.max(leaves, function(d){
+            	return d.Sales
+            })
+            var year = d3.nest().key(function(d){
+            	return d.Year
+            })
+            .entries(leaves);
+            return {max:max, year:year};
+            })
 	  .entries(data)
 
   // Scale the range of the data
   x.domain(d3.extent(data, function(d) { return d.Month; }));
-  y.domain([0, d3.max(data, function(d) { return d.Sales; })]);
+  //y.domain([0, d3.max(data, function(d) { return d.Sales; })]);
   
   // Set up the x axis
   var xaxis = svg.append("g")
@@ -68,26 +75,9 @@ d3.csv("Example4.csv", function(error, data) {
           .tickSizeInner(0)
           .tickPadding(10));
 
-  // Add the Y Axis
-   var yaxis = svg.append("g")
-       .attr("class", "y axis")
-       .call(d3.axisLeft(y)
-          .ticks(5)
-          .tickSizeInner(0)
-          .tickPadding(6)
-          .tickSize(0, 0));
-  
-  // Add a label to the y axis
-  svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - 60)
-        .attr("x", 0 - (height / 2))
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        .text("Monthly Sales")
-        .attr("class", "y axis label");
 
-  // Create a dropdown
+
+  // Create 1st dropdown
     var fruitMenu = d3.select("#fruitDropdown")
 
     fruitMenu
@@ -103,6 +93,22 @@ d3.csv("Example4.csv", function(error, data) {
             return d.key;
         })
 
+    // Create 2nd dropdown
+    var yearMenu = d3.select("#yearDropdown")
+
+    yearMenu
+    	.data(nest)
+		.append("select")
+		.selectAll("option")
+        .data(function(d) { return d.value.year; })
+       	.enter()
+        .append("option")
+        .attr("value", function(d){
+            return d.key;
+        })
+        .text(function(d){
+            return d.key;
+        })
 
  
  	// Function to create the initial graph
@@ -120,9 +126,12 @@ d3.csv("Example4.csv", function(error, data) {
 		    .enter()
 		    .append("g")
 		    .attr("class", "fruitGroups")
+		    .each(function(d){
+                y.domain([0, d.value.max])
+            });
 
 		var initialPath = selectFruitGroups.selectAll(".line")
-			.data(function(d) { return d.values; })
+			.data(function(d) { return d.value.year; })
 			.enter()
 			.append("path")
 
@@ -131,6 +140,25 @@ d3.csv("Example4.csv", function(error, data) {
 				return valueLine(d.values)
 			})
 			.attr("class", "line")
+
+		  // Add the Y Axis
+		   var yaxis = svg.append("g")
+		       .attr("class", "y axis")
+		       .call(d3.axisLeft(y)
+		          .ticks(5)
+		          .tickSizeInner(0)
+		          .tickPadding(6)
+		          .tickSize(0, 0));
+		  
+		  // Add a label to the y axis
+		  svg.append("text")
+		        .attr("transform", "rotate(-90)")
+		        .attr("y", 0 - 60)
+		        .attr("x", 0 - (height / 2))
+		        .attr("dy", "1em")
+		        .style("text-anchor", "middle")
+		        .text("Monthly Sales")
+		        .attr("class", "y axis label");
 
  	}
 
@@ -149,17 +177,29 @@ d3.csv("Example4.csv", function(error, data) {
  		// Select all of the grouped elements and update the data
 	    var selectFruitGroups = svg.selectAll(".fruitGroups")
 		    .data(selectFruit)
+		    .each(function(d){
+                y.domain([0, d.value.max])
+            });
 
 		    // Select all the lines and transition to new positions
             selectFruitGroups.selectAll("path.line")
-               .data(function(d){
-                  return (d.values);
-                })
-                .transition()
+               .data(function(d) { return d.value.year; }, 
+               		function(d){ return d.key; })
+               .transition()
                   .duration(1000)
                   .attr("d", function(d){
                     return valueLine(d.values)
                   })
+
+        // Update the Y-axis
+            d3.select(".y")
+                    .transition()
+                    .duration(1500)
+                    .call(d3.axisLeft(y)
+                      .ticks(5)
+                      .tickSizeInner(0)
+                      .tickPadding(6)
+                      .tickSize(0, 0));
 
 
  	}
@@ -175,5 +215,31 @@ d3.csv("Example4.csv", function(error, data) {
 
         // Run update function with the selected fruit
         updateGraph(selectedFruit)
+
+
     });
+
+
+    // Change color of selected line when year dropdown changes
+    yearMenu.on('change', function(){
+
+    	// Find which year was selected
+    	var selectedYear = d3.select(this)
+    		.select("select")
+    		.property("value")
+
+    	// Change the class of the matching line to "selected"
+    	var selLine = svg.selectAll(".line")
+              // de-select all the lines
+              .classed("selected", false)
+              .filter(function(d) {
+                  return +d.key === +selectedYear
+              })
+              // Set class to selected for matching line
+              .classed("selected", true)
+              .raise()
+    })
+
+
+  
 })
